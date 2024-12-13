10c2357d1f3c956123ea243285793e16e405ca7e
Joonas Tuomi <jtuomi@student.hive.fi>
2024-12-13 21:04:25 +0200

    Starting...

--- pipex.c
+++ pipex.c
@@ -1,11 +1,17 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "pipex.h"
+#include "libft/libft.h"
 //
 // Created by jtuomi on 12/6/24.
 //
 int main(int argc, char *argv[]) {
   int fd[2];
 
-  if (argc < 2) {
-    ft_printf("Usage: pipex <command>\n");
+  if (argc < 5) {
+    ft_printf("Usage: pipex <file> <command> <command> <file>\n");
     return 1;
   }
   pid_t pid;
@@ -13,4 +19,5 @@ int main(int argc, char *argv[]) {
     perror("pipe");
     exit(1);
   }
+  pid = fork();
 }
