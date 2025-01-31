219eea5e7ebd2855b857591b2e5bd20eb18e73bf
Joonas Tuomi <jtuomi@student.hive.fi>
2025-01-31 19:33:02 +0200

    Wrote some boilerplate code.

--- pipex.c
+++ pipex.c
@@ -1,23 +1,80 @@
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include "pipex.h"
-#include "libft/libft.h"
-//
-// Created by jtuomi on 12/6/24.
-//
-int main(int argc, char *argv[]) {
-  int fd[2];
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtuomi <jtuomi@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/16 10:23:10 by jtuomi            #+#    #+#             */
+/*   Updated: 2025/01/31 13:08:59 by jtuomi           \__/    i               */
+/*                                                                            */
+/* ************************************************************************** */
 
-  if (argc < 5) {
-    ft_printf("Usage: pipex <file> <command> <command> <file>\n");
-    return 1;
-  }
-  pid_t pid;
-  if (pipe(fd) < 0) {
-    perror("pipe");
-    exit(1);
-  }
-  pid = fork();
+#include "libft/libft.h"
+#include "pipex.h"
+
+static void	validate_open_pipe_pid(int fd[2], int pipefd, pid_t pid);
+static void	plumbing(int fd[2], int pipefd[2], pid_t pid, char **argv);
+
+int	main(int argc, char *argv[])
+{
+	int		fd[2];
+	int		pipefd[2];
+	int		check;
+	pid_t	pid[2];
+
+	if (argc < 5)
+	{
+		perror("Usage: pipex <file> <command> ... <command> <file>\n");
+		exit(EXIT_FAILURE);
+	}
+	fd[0] = open(argv[1], O_RDWR);
+	fd[1] = open(argv[argc - 1], O_RDWR);
+	check = pipe(pipefd);
+	pid[0] = fork();
+	pid[1] = fork();
+	validate_open_pipe_pid(fd, check, pid);
+	plumbing(fd, pipefd, pid, &argv[1])
+}
+
+t_command	new_command(t_command command)
+{
+	command = (t_command)
+	{
+		.path = arr[BUFSIZ], .args = NULL, arg_len = 0, .fd_in = STDIN_FILENO,
+			.fd_out = STDIN_FILENO
+	}
+	return (command);
+}
+
+static void	plumbing(int fd[2], int pipefd[2], pid_t pid, char **argv)
+{
+	// TODO: pipe file 1 to stdout for execve of cmd 1
+	// TODO: pipe output of cmd 1 to stdout for cmd 2 to read
+	// TODO: read stdin and write to file 2
+}
+
+static void	validate_open_pipe_pid(int fd[2], int pipefd, pid_t pid)
+{
+	if (fd[0] < 0 || fd[1] < 0)
+	{
+		if (fd[1] > -1)
+			close(fd[1]);
+		if (fd[0] > -1)
+			close(fd[0]);
+		perror("open");
+		exit(EXIT_FAILURE);
+	}
+	else if (pipefd == -1)
+	{
+		close(fd[1]);
+		close(fd[0]);
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
 }
--- pipex.h
+++ pipex.h
@@ -1,13 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                     __     h  _ _  _| _    */
+/*   pipex.h                                        __/  \__  i (_(_)(_|(/_   */
+/*                                                 /  \__/  \ v  _ _|_        */
+/*   By: jtuomi <jtuomi@student.hive.fi>           \__/  \__/ e _>(_| | --    */
+/*                                                 /  \__/  \ .  _  _ |       */
+/*   Created: 2025/01/31 12:59:37 by jtuomi        \__/  \__/ f (_)(_)|       */
+/*   Updated: 2025/01/31 13:05:05 by jtuomi           \__/    i               */
+/*                                                                            */
+/* ************************************************************************** */
 #ifndef PIPEX_H
-#define PIPEX_H
-#include <errno.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+# define PIPEX_H
+# include <errno.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
-int open_pipex(const char *pipex_path);
-void close_pipex(int fd);
-int read_pipex(int fd, void *buf, size_t count);
-int write_pipex(int fd, const void *buf, size_t count);
+typedef struct s_command
+{
+	char	*path;
+	char	**args;
+	int		arg_len;
+	int		fd_in;
+	int		fd_out;
+}			t_command;
+
+t_command	new_command(t_command command);
+int			open_pipex(const char *pipex_path);
+void		close_pipex(int fd);
+int			read_pipex(int fd, void *buf, size_t count);
+int			write_pipex(int fd, const void *buf, size_t count);
 #endif
